c2202b67132b1151d95c75b6cbbe473cf558938c
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-09-12 22:18:43 -0500

    adding response int and type assertions

--- __tests__/test_the_fixture.py
+++ __tests__/test_the_fixture.py
@@ -3,16 +3,33 @@ from webtest_flask_fixture import test_app
 
 
 def test_can_load_test_index(mock_site, test_app):
-    index = test_app.get('/')
-    assert 'Hello, World!' in index  # string must be present in body
+    # Act
+    resp = test_app.get('/')
+
+    # Assert
+    assert resp.status_int == 200
+    assert resp.content_type == 'text/html'
+    assert 'Hello, World!' in resp  # string must be present in body
 
 
 def test_can_load_test_js_file(mock_site, test_app):
-    script = test_app.get('/scripts/script.js')
-    assert 'console.log(' in script  # string must be present in body
+    # Act
+    resp = test_app.get('/scripts/script.js')
+
+    # Assert
+    assert resp.status_int == 200
+    assert resp.content_type == 'application/javascript'
+    assert 'console.log(' in resp  # string must be present in body
 
 
 def test_can_press_test_button(mock_site, test_app):
-    page = test_app.get('/')
-    page = page.click('Page 2')
-    assert 'Page Two' in page
+    # Arrange
+    resp = test_app.get('/')
+
+    # Act
+    resp = resp.click('Page 2')
+
+    # Assert
+    assert resp.status_int == 200
+    assert resp.content_type == 'text/html'
+    assert 'Page Two' in resp
