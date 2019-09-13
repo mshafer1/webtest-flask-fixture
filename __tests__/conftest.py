54ccd6c672d3751031bd521f3fadf8c1c4937dcc
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-09-12 22:06:54 -0500

    get mocking of app working

--- __tests__/conftest.py
+++ __tests__/conftest.py
@@ -5,5 +5,6 @@ _base_dir = os.path.dirname(os.path.realpath(__file__))
 
 @pytest.fixture
 def mock_site(mocker):
-    mock = mocker.patch('webtest_flask_fixture._get_cwd', return_value=os.path.join(_base_dir, 'test_files'))
+    import webtest_flask_fixture.test_app_mod
+    mock = mocker.patch('webtest_flask_fixture.test_app_mod._get_cwd', return_value=os.path.join(_base_dir, 'test_files'))
     return mock
